c5af3c483923ca148bff675016871578b85a51f4
Leandro Castillo Borja <lcastillo@peruapps.com.pe>
2021-02-04 01:03:44 -0500

    Crear método de leer y guardar string

--- app/src/main/java/com/leandro1995/datastore/application/App.kt
+++ app/src/main/java/com/leandro1995/datastore/application/App.kt
@@ -2,12 +2,16 @@ package com.leandro1995.datastore.application
 
 import android.app.Application
 import com.leandro1995.datastore.datastore.DataStore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        DataStore.instance(context = applicationContext)
+        GlobalScope.launch {
+            DataStore.instance(context = applicationContext)
+        }
     }
 }
--- app/src/main/java/com/leandro1995/datastore/datastore/DataStore.kt
+++ app/src/main/java/com/leandro1995/datastore/datastore/DataStore.kt
@@ -3,15 +3,35 @@ package com.leandro1995.datastore.datastore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.createDataStore
+import kotlinx.coroutines.flow.first
 
 
 object DataStore {
     private lateinit var dataStore: DataStore<Preferences>
+    private lateinit var dataStoreKey: Preferences.Key<String>
+    private lateinit var preferences: Preferences
 
     private const val DATA_STORE_NAME = "data_store"
+    private const val MESSAGE = "message"
 
-    fun instance(context: Context) {
+    suspend fun instance(context: Context) {
         dataStore = context.createDataStore(name = DATA_STORE_NAME)
+        preferences = dataStore.data.first()
+    }
+
+    suspend fun setMessage(value: String) {
+        dataStoreKey = stringPreferencesKey(MESSAGE)
+
+        dataStore.edit { preference ->
+            preference[dataStoreKey] = value
+        }
+    }
+
+    fun getMessage(): String? {
+        dataStoreKey = stringPreferencesKey(MESSAGE)
+        return preferences[dataStoreKey]
     }
 }
